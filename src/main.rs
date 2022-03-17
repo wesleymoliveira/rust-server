397edd05149c30f24be734be93c39da09a15e24c
Wesley M. Oliveira <wesley@sortium>
2022-03-17 10:59:29 -0300

    feat: add simple request validation and 404 page

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,32 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n"; //hard-coded request line prefixed by b representing byte array of the string
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Lenght: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Lenght: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "{}\r\nContent-Lenght: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
